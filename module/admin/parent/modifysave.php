bb631d9636cd1cdb48da9c80807281b2171a1552
Sagor Mollik <58267879+sagor-mollik@users.noreply.github.com>
2019-11-28 02:21:43 +0600

    Delete modifysave.php

--- module/admin/parent/modifysave.php
+++
@@ -1,7 +0,0 @@
-<?php  
-include_once('main.php');
-$password=$_REQUEST['password'];
-$mod = "UPDATE parents,users SET parents.password='$password',users.password = '$password' WHERE parents.id = users.userid AND parents.id ='$check'";
-$resmon = mysql_query($mod);
-header('location:modify.php');
-?>
